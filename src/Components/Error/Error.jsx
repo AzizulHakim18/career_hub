73313e6664404a2c4ad2997367d574a21dc9e783
AzizulHakim18 <hakimrony18@gamil.com>
2024-04-28 23:08:55 +0600

    first commit

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,10 @@
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Oop!!! 404 this page is not found</h1>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root';
+import Error from './Components/Error/Error';
+import Home from './Components/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
